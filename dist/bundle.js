f2fda9f596dac65714b6a05cc111809ac7b367c4
Dang Hai Trieu <dangtrieu25@gmail.com>
2018-03-03 18:08:13 +0700

    add command instruction, enhance app behavior

--- dist/bundle.js
+++ dist/bundle.js
@@ -55,9 +55,41 @@ exports.homedir = function () {
 
   const outputEl = document.getElementById('output-content')
   const generateAndCopyBtn = document.getElementById('btn-generate-and-copy')
+  const countInputBox = document.getElementById('count')
+  const unitSelectBox = document.getElementById('unit')
   const labelCopied = document.getElementById('label-copied')
+  const toggleInstructionsBtn = document.getElementById('btn-toggle-instructions')
 
   let currentTimeout
+  let countChanged = false
+
+  toggleInstructionsBtn.addEventListener('click', function () {
+    const instructionsEl = document.getElementById('instructions')
+    instructionsEl.classList.toggle('hidden')
+  })
+
+  countInputBox.addEventListener('input', function (event) {
+    countChanged = true
+  })
+
+  unitSelectBox.addEventListener('change', function (event) {
+    if (countChanged) {
+      return
+    }
+    const units = event.target.value
+    
+    switch (units) {
+      case 'words':
+        countInputBox.value = 20
+        break
+      case 'sentences':
+        countInputBox.value = 5
+        break
+      case 'paragraphs':
+        countInputBox.value = 2
+        break
+    }
+  })
 
   generateAndCopyBtn.addEventListener('click', function () {
     const countEl = document.getElementById('count')
